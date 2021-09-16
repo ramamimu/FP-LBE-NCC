89eacacf2662249c8dda633c82aabba64b711392
ramamimu <ahhmaad.blogger@gmail.com>
2021-09-16 18:45:04 +0700

    inisial commit

---
+++ learn/coba_py.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+with open("//home//ramamimu//code//web_scrape//index.html", 'r') as html_file:
+    content = html_file.read()
+    # print(content)
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())
+    tags=soup.find('p')
+    print(tags)
