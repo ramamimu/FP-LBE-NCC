159b3058ce46b6d984fa5b1fe67a0a15d4dc0de8
ramamimu <ahhmaad.blogger@gmail.com>
2021-09-17 20:42:27 +0700

    :art: delete cache and improve py code

--- learn/coba_py.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("//home//ramamimu//code//web_scrape//learn//index.html", 'r') as html_file:
-    content = html_file.read()
-    # print(content)
-    soup = BeautifulSoup(content, 'lxml')
-    # print(soup.prettify())
-    tags=soup.find('p')
-    print(tags)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
+from mysql.connector.errors import DatabaseError
 import requests
+import mysql.connector
 
 html_text = requests.get('https://www.gensh.in/characters')
 print(html_text)
-soup = BeautifulSoup(html_text.text, "lxml")
+soup = BeautifulSoup(html_text.text, "html.parser")
 # images = soup.find_all('div', class_="character-card col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-4 card-outer mb-4")
 
 def desc_link():
@@ -18,9 +20,10 @@ def desc_link():
     i = 0
     while (i<index):
         desc_link[i]="https:/www.gensh.in"+desc_link[i].a['href']
-        print(desc_link[i])
+        # print(desc_link[i])
         i+=1
     # print (desc_link)
+    return desc_link
 
 def images_ch():
     global soup
@@ -35,8 +38,9 @@ def images_ch():
     i = 0
     while (i<index_ch):
         images_ch[i]="https:/www.gensh.in"+images_ch[i]['src']
-        print(images_ch[i])
+        # print(images_ch[i])
         i+=1
+    return images_ch
 
 def name_ch():
     global soup
@@ -50,9 +54,38 @@ def name_ch():
     while (i<index_name):
         name_ch[i]=name_ch[i].h2.text
         # print(name_ch[i].h2.text)
-        print(name_ch[i])
+        # print(name_ch[i])
         i+=1
+    return name_ch
 
 # desc_link()
 # images_ch()
 # name_ch()
+
+description = desc_link()
+images = images_ch()
+names = name_ch()
+
+# print (description)
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    username = "root",
+    password = "",
+    database = "wibu-chan"
+)
+
+my_cursor = mydb.cursor()
+# sql = "INSERT INTO character_genshin (name, image, description) VALUES (%s,%s,%s)"
+# val = ("aiudhuh", "akfiush", "fhsjsf")
+my_cursor.execute("INSERT INTO `character_genshin` (`name`, `image`, `description`) VALUES ('satu', 'dua', 'tiga'), ('', '', '')")
+
+# INSERT INTO `character_genshin` (`name`, `image`, `description`) VALUES ('dsgwerwer', 'wer', 'werwerwe'), ('', '', '');
+
+# my_cursor.execute(sql)
+# my_cursor.execute("INSERT INTO character_genshin (name,image,description) VALUES (qwer,qwrqwe,grdr)")
+
+mydb.commit()
+# print(mydb)
+# test = mydb.cursor()
+# test.execute("tes 1 bang aslkdja")
