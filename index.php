c6dbaab69d48f85acec3f4946df2f1936368ba45
ramamimu <ahhmaad.blogger@gmail.com>
2021-09-18 09:36:24 +0700

    backup db connection

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Waifu Spinner</title>
+    <link rel="stylesheet" href="assets/style.css" />
+  </head>
+  <body>
+    <div class="content-main">
+      <nav>
+        <h2>Waifu Spinner</h2>
+      </nav>
+      <main>
+        <div class="spinner">
+          <div class="container">
+            <div class="spinner-content">
+              <img id="waifu-img" src="assets/img/genshin-1.jpg" alt="" />
+              <button>Spin Your Waifu</button>
+            </div>
+          </div>
+        </div>
+        <div class="image-main">sdf</div>
+      </main>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
